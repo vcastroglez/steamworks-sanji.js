5b8a46f23482580a26c6d6fa9b51a789927af6c5
Sanji <sanji@chibiphoenix.com>
2023-05-09 10:02:19 +0300

    mychanges

--- test/electron/renderer.js
+++ test/electron/renderer.js
@@ -1,6 +1,31 @@
 /** @type {import('steamworks.js')} */
-const steamworks = require('steamworks.js');
-const client = steamworks.init(480);
+const steamworks = require('steamworks.js')
+const client = steamworks.init(480)
 
 const playerName = client.localplayer.getName()
 document.getElementById('name').innerText = playerName
+
+let canva1_left = 0;
+let canva2_right = -1;
+function refresh() {
+    const width = document.body.clientWidth;
+    if(canva2_right==-1) {
+        canva2_right = width
+    }
+    const canva1 = document.getElementById('top-left')
+    const canva2 = document.getElementById('bottom-right')
+    if(!canva1) return;
+    canva1_left += 1;
+    if(canva1_left>=width)
+        canva1_left=0;
+    canva1.style.left = canva1_left+'px';
+
+    canva2_right += 1;
+    if(canva2_right>=width)
+        canva2_right = 0
+    canva2.style.right = canva2_right+'px';
+    requestAnimationFrame(refresh)
+}
+document.addEventListener("readystatechange", (event) => {
+    refresh()
+});
